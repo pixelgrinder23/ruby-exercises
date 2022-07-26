c4b75fd186f71be6e2be7c31cd2e33543f6573a9
Angela Dazzle <angela@pixelgrinder.co.uk>
2022-07-26 06:19:18 +0100

    Ruby started & initial exercises

--- ruby_basics/1_data_types/exercises/numbers_exercises.rb
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -1,36 +1,59 @@
 def add(a, b)
   # return the result of adding a and b
-  a + b
+  a + b 
 end
 
 def subtract(a, b)
   # return the result of subtracting b from a
+  a - b
 end
 
 def multiply(a, b)
   # return the result of multiplying a times b
+  a * b
 end
 
 def divide(a, b)
   # return the result of dividing a by b
+  a / b
 end
 
 def remainder(a, b)
   # return the remainder of dividing a by b using the modulo operator
+  a % b
+
 end
 
 def float_division(a, b)
   # return the result of dividing a by b as a float, rather than an integer
+  a / b.to_f
 end
 
 def string_to_number(string)
   # return the result of converting a string into an integer
+  string.to_i
 end
 
 def even?(number)
   # return true if the number is even (hint: use integer's even? method)
+  number.even?
 end
 
 def odd?(number)
   # return true if the number is odd (hint: use integer's odd? method)
+  number.odd?
 end
+
+add(1, 2)
+subtract(5, 3)
+multiply(5, 5)
+divide(25, 5)
+remainder(25, 5)
+remainder(13, 5)
+float_division(10, 2)
+string_to_number('1')
+string_to_number('-5')
+even?(6)
+even?(5)
+odd?(9)
+odd?(6)
--- ruby_basics/1_data_types/exercises/string_exercises.rb
+++ ruby_basics/1_data_types/exercises/string_exercises.rb
@@ -1,40 +1,69 @@
 def concatenate_example(string)
   # use concatenation to format the result to be "Classic <string>"
-  "Classic " << string
+  "Classic " << string 
 end
 
 def concatenate(string)
   # use concatenation to format the result to be "Hello <string>!"
+  "Hello " + string + "!"
 end
 
 def substrings(word)
   # return the first 4 letters from the word using substrings
+  word[0..3]
 end
 
 def capitalize(word)
   # capitalize the first letter of the word
+  word.capitalize
 end
 
 def uppercase(string)
   # uppercase all letters in the string
+  string.upcase
 end
 
 def downcase(string)
   # downcase all letters in the string
+  string.downcase
 end
 
 def empty_string(string)
   # return true if the string is empty
+  string.empty?
 end
 
 def string_length(string)
   # return the length of the string
+  string.length
 end
 
 def reverse(string)
   # return the same string, with all of its characters reversed
+  string.reverse
 end
 
 def space_remover(string)
   # remove all the spaces in the string using gsub
+  string.gsub(" ", "")
 end
+
+concatenate_example("Ruby")
+concatenate_example("world")
+concatenate_example("universe")
+substrings("chocloate")
+capitalize("paris")
+capitalize("miami in the summer")
+capitalize("London")
+uppercase("small")
+uppercase("make me bigger")
+downcase("large")
+downcase("MAKE ME SMALLER")
+empty_string("")
+empty_string("wow")
+string_length("longitude")
+string_length("i am quite long")
+reverse("desrever")
+reverse("dnuora kcab")
+space_remover("white space")
+space_remover("many white spaces")
